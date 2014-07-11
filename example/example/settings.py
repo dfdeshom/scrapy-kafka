9ba4e77326f41b67afec2d7e0f093065c8e6f532
dfdeshom <didier@cogtree.com>
2014-07-11 17:26:08 -0500

    add example scrapy project to demonstrate use of pipeline

---
+++ example/example/settings.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for example project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'example'
+
+SPIDER_MODULES = ['example.spiders']
+NEWSPIDER_MODULE = 'example.spiders'
+ITEM_PIPELINES = [
+        'example.pipelines.ExamplePipeline',
+        'scrapy_kafka.pipelines.KafkaPipeline',
+    ]
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'example (+http://www.yourdomain.com)'
+    
