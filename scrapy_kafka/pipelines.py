135066057796baeaab4c9393017f6e7ce8d62337
dfdeshom <didier@cogtree.com>
2014-07-11 23:33:43 -0500

    move pipeline-related settings to one file

--- scrapy_kafka/pipelines.py
+++ scrapy_kafka/pipelines.py
@@ -1,11 +1,14 @@
-from scrapy_kafka.connection import from_settings
 from scrapy.utils.serialize import ScrapyJSONEncoder
+
+from kafka.client import KafkaClient
+from kafka.producer import SimpleProducer
 
 class KafkaPipeline(object):
     """Publishes a serialized item into a Kafka toppic"""
     def __init__(self, producer, topic):
         self.producer = producer
         self.topic = topic
+        
         self.encoder = ScrapyJSONEncoder()
         
     def process_item(self, item, spider):
@@ -17,6 +20,9 @@ class KafkaPipeline(object):
     
     @classmethod
     def from_settings(cls, settings):
-        conn, topic = from_settings(settings)
+        k_hosts = settings.get('SCRAPY_KAFKA_HOSTS',['localhost:9092'])
+        topic = settings.get('KAFKA_ITEM_PIPELINE_TOPIC','scrapy_kafka_item')
+        kafka = KafkaClient(k_hosts)
+        conn = SimpleProducer(kafka)
         return cls(conn,topic)
     
